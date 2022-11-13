3f4ec60b84e24fad8772996ad6f9d01e7390bafa
Nandini K_Gowda <117714189+Hello-Learner@users.noreply.github.com>
2022-11-13 19:01:19 +0530

    Add files via upload

---
+++ Java/oop.java
@@ -0,0 +1,56 @@
+/*class oop{
+    int x=4;
+    public static void main(String args[])
+    {
+        oop nan= new oop();
+        oop nan1=new oop();
+        System.out.println(nan.x);
+        System.out.println(nan1.x);
+    }
+}*/
+/* class attribute
+class oop{
+    String a;
+    public static void main(String args[])
+    {
+        
+    oop simple=new oop();
+    simple.a="Nandini";
+    System.out.println(simple.a);
+    }
+}*/
+/*class Method
+public class oop {
+  // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+  // Main method
+  public static void main(String[] args) {
+    myStaticMethod(); // Call the static method
+    // myPublicMethod(); This would compile an error
+
+    oop myObj = new oop(); // Create an object of Main
+    myObj.myPublicMethod(); // Call the public method on the object
+  }
+}*/
+/*java constructor
+public class oop{
+    int x;
+    public oop(){//same name as class name is "constructor"
+     x=5;
+    }
+    
+    public static void main(String args[])
+    {
+        oop simple=new oop();
+        System.out.println(simple.x);
+    
+}
+}*/
